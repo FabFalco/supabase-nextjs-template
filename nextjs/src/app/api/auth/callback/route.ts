d61507895f5c0a0cd6ddaf3bb06019832667e711
FabFalco <fabfalco.ff@gmail.com>
2025-10-31 14:24:49 +0100

    Upgrade stripe

--- nextjs/src/app/api/auth/callback/route.ts
+++ nextjs/src/app/api/auth/callback/route.ts
@@ -4,7 +4,7 @@ import { createSSRSassClient } from "@/lib/supabase/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-09-30.clover"
+  apiVersion: "2025-10-29.clover"
 })
 
 //export const dynamic = "force-dynamic"; // ✅ important pour Next.js 15+
