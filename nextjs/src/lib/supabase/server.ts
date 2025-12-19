1dd3e740d18cbd91ff9bac758b07c1818844f848
FabFalco <fabfalco.ff@gmail.com>
2025-12-19 12:24:15 +0100

    another any coockie

--- nextjs/src/lib/supabase/server.ts
+++ nextjs/src/lib/supabase/server.ts
@@ -1,6 +1,13 @@
 import {createServerClient} from '@supabase/ssr'
 import {cookies} from 'next/headers'
 import {ClientType, SassClient} from "@/lib/supabase/unified";
+import type { CookieOptions } from '@supabase/ssr'
+
+type CookieToSet = {
+  name: string
+  value: string
+  options?: CookieOptions
+}
 
 export async function createSSRClient() {
     const cookieStore = await cookies()
@@ -13,7 +20,7 @@ export async function createSSRClient() {
                 getAll() {
                     return cookieStore.getAll()
                 },
-                setAll(cookiesToSet) {
+                setAll(cookiesToSet: CookieToSet[]) {
                     try {
                         cookiesToSet.forEach(({ name, value, options }) =>
                             cookieStore.set(name, value, options)
